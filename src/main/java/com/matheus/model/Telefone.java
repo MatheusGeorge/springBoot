81d7e7ed4af715cac7e95db7eaa3df920d9f4993
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2017-05-16 18:48:15 -0300

    Criaçao das classes usuario e telefone

---
+++ src/main/java/com/matheus/model/Telefone.java
@@ -0,0 +1,47 @@
+package com.matheus.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String numero;
+    private String descricao;
+    @ManyToOne
+    private Usuario usuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ src/main/java/com/matheus/model/Usuario.java
@@ -0,0 +1,48 @@
+package com.matheus.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
+    private List<Telefone> telefones;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+}
